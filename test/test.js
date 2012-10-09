ea865824e101ea8e1cf03912850ac9de9ebc0795
Dan VerWeire <dverweire@gmail.com>
2012-10-09 11:00:48 -0400

    added better tests using nodeunit

--- test/test.js
+++ test/test.js
@@ -1,52 +1,127 @@
 var Queue = require('../');
 
-var q = new Queue();
+exports['chainish-context'] = function (test) {
+	var context = { a : 'hello' };
 
-q.add(function ( next ) {
-	console.log('hello');
-	return next();
-});
+	Queue({ context : context})
+	(function (next) {
+		test.equal(this, context)
+		return next();
+	})
+	(function (next) {
+		test.equal(this, context);
+		return next();
+	})
+	(function () {
+		return test.done();	
+	})();
+};
 
-q.add(function ( next ) {
-	console.log('world');
+exports['chainish-named-queues'] = function (test) {
+	var a = [];
 
-	return next();
-});
+	Queue()
+	('first', function (next) {
+		a.push(1);
+		return next();
+	})
+	('second', function (next) {
+		a.push(2);
+		return next();
+	})
+	('first', function (next) {
+		a.push(1);
+		return next();
+	})
+	('last', function (next) {
+		a.push(3);
 
-q.add(function () {
-	console.log('done');
-});
+		test.deepEqual(a, [1,1,2,3]);
+		test.done();
+	})
+	();
+};
 
-q.execute();
+exports['object-instantiate-add-execute'] = function (test) {
+	a = [];
 
-
-(new Queue())
-
-(function ( next ) {
-	setTimeout(function () {
-		
-		console.log('hello 2');
-		
-		return next('world 2');
-		
-	}, 1000);
-})
-
-(function ( world, next ) {
-	setTimeout(function () {
-		
-		console.log(world);
+	var q = new Queue();
+	
+	q.add(function (next) {
+		a.push(1);
 
 		return next();
-		
-	}, 1000);
-	
-	
-})
+	});
 
-(function () {
+	q.add(function (next) {
+		a.push(2);
+
+		return next();
+	});
+
+	q.add(function (next) {
+		a.push(3);
+		
+		test.deepEqual(a, [1,2,3]);
+		test.done();
+	});
+
+	q.execute();
+
 	
-	console.log('done 2');
-	
-	
-})();
+};
+
+exports['nothing'] = function (test) {
+	return test.done()
+	var q = new Queue();
+
+	q.add(function ( next ) {
+		console.log('hello');
+		return next();
+	});
+
+	q.add(function ( next ) {
+		console.log('world');
+
+		return next();
+	});
+
+	q.add(function () {
+		console.log('done');
+	});
+
+	q.execute();
+
+
+	(new Queue())
+
+	(function ( next ) {
+		setTimeout(function () {
+			
+			console.log('hello 2');
+			
+			return next('world 2');
+			
+		}, 1000);
+	})
+
+	(function ( world, next ) {
+		setTimeout(function () {
+			
+			console.log(world);
+
+			return next();
+			
+		}, 1000);
+		
+		
+	})
+
+	(function () {
+		
+		console.log('done 2');
+		
+		
+	})();
+
+};
